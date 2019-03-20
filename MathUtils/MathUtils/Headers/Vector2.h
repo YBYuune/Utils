8b500c49467ada190da8245ada33e305bf197289
Yugo Brunet <yugobrunet@gmail.com>
2019-03-20 11:14:07 -0400

    Added Visual Studio project and started on Vector2 struct

---
+++ MathUtils/MathUtils/Headers/Vector2.h
@@ -0,0 +1,28 @@
+#ifndef __VECTOR2_H__
+#define __VECTOR2_H__
+
+
+struct Vector2
+{
+public:
+	Vector2();
+	Vector2(float x);
+	Vector2(float x, float y);
+	~Vector2();
+
+
+
+
+
+
+
+////      ---- MEMBER VARIABLES ----
+public:
+	float x;
+	float y;
+
+};
+
+
+
+#endif
