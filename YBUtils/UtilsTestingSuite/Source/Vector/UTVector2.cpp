0309f6782d342cce76f469b749f8ab29f8e36bdb
Yugo Brunet <yugobrunet@gmail.com>
2019-03-21 00:48:32 -0400

    Updated VS Project settings to properly create precompiled header, as well as located additional includes. Added Unit Test cases for the different constructors of Vector2.

--- YBUtils/UtilsTestingSuite/Source/Vector/UTVector2.cpp
+++ YBUtils/UtilsTestingSuite/Source/Vector/UTVector2.cpp
@@ -1,23 +1,48 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "..\MathUtils\Source Files\Vector\Vector2.h"
+#include "Vector\Vector2.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace YBMath;
 
-namespace UtilsTestingSuite
+namespace YBMathTestingSuite
 {		
 	TEST_CLASS(Vector2)
 	{
 	public:
-		
-		TEST_METHOD(CreateVector2)
+		TEST_METHOD(CreateVector2_ZeroParam)
 		{
-            Logger::WriteMessage("IN: CreateVector2");
+            Logger::WriteMessage("IN: CreateVector2_ZeroParam");
             YBMath::Vector2 testVec;
 
-            Assert::AreEqual(testVec.x, 0.0f);
-            Assert::AreEqual(testVec.y, 0.0f);
+            Assert::AreEqual(testVec.GetX(), 0.0f);
+            Assert::AreEqual(testVec.GetY(), 0.0f);
+		}
+
+		TEST_METHOD(CreateVector2_OneParam)
+		{
+			Logger::WriteMessage("IN: CreateVector2_OneParam");
+
+			float xyValue = 5.5f;
+
+			YBMath::Vector2 testVec(xyValue);
+
+			Assert::AreEqual(testVec.GetX(), xyValue);
+			Assert::AreEqual(testVec.GetY(), xyValue);
+		}
+
+
+		TEST_METHOD(CreateVector2_TwoParam)
+		{
+			Logger::WriteMessage("IN: CreateVector2_TwoParam");
+
+			float xValue = 5.5f;
+			float yValue = -4.3f;
+
+			YBMath::Vector2 testVec(xValue, yValue);
+
+			Assert::AreEqual(testVec.GetX(), xValue);
+			Assert::AreEqual(testVec.GetY(), yValue);
 		}
 
 	};
